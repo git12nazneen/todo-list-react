4dc8a390be9d45afa9c2e425f4d9e117e6d2eb71
git12nazneen <lipinazneen65@gmail.com>
2025-02-03 00:15:45 +0600

    value set

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react'
 
 const TodoForm = () => {
   const [value, setValue] = useState("")
+  const handleSubmit = e =>{
+    e.preventDefault()
+    console.log('Form submitted with value: ', value)
+  
+  }
   return (
-    <form className='TodoForm'>
-      <input type="text" placeholder='write your todo' className='todo-input' onChange={(e)=>console.log(e.target.value)}  />
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      <input type="text" placeholder='write your todo' className='todo-input' onChange={(e)=>setValue(e.target.value)}  />
       <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
